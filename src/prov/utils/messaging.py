f6783a4436c55b20630b541168cdb3f6d3036ea5
blankdots <stefan@blankdots.com>
2017-09-22 09:26:28 +0300

    ATTX-project/provenance-service#12 final adjustments

--- src/prov/utils/messaging.py
+++ src/prov/utils/messaging.py
@@ -33,8 +33,8 @@ class Consumer(object):
                 self.connection = Connection(self.hostname, self.username, self.password)
                 app_logger.info('Established connection with AMQP server {0}'.format(self.connection))
                 break
-            except amqpstorm.AMQPError as why:
-                app_logger.error(why)
+            except amqpstorm.AMQPError as error:
+                app_logger.error('Something went wrong: {0}'.format(error))
                 if self.max_retries and attempts > self.max_retries:
                     break
                 time.sleep(min(attempts * 2, 30))
@@ -57,8 +57,8 @@ class Consumer(object):
                 channel.start_consuming(to_tuple=False)
                 if not channel.consumer_tags:
                     channel.close()
-            except amqpstorm.AMQPError as why:
-                app_logger.error(why)
+            except amqpstorm.AMQPError as error:
+                app_logger.error('Something went wrong: {0}'.format(error))
                 self.create_connection()
             except KeyboardInterrupt:
                 self.connection.close()
@@ -79,5 +79,5 @@ class Consumer(object):
                 result = {'task_id': tasks}
             message.ack()
             app_logger.info('Processed provenance message with result {0}.'.format(result))
-        except Exception as e:
-            app_logger.error('Something went wrong: {0}'.format(e))
+        except Exception as error:
+            app_logger.error('Something went wrong: {0}'.format(error))
--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -2,11 +2,13 @@ import unittest
 # from rdflib import Graph
 from mock import patch
 from prov.utils.messaging import Consumer
-from prov.utils.broker import broker
-from pytest_rabbitmq import factories
-
-rabbitmq_my_proc = factories.rabbitmq_proc(port=5672, logsdir='/tmp')
-rabbitmq_my = factories.rabbitmq('rabbitmq_my_proc')
+# from amqpstorm import Connection
+# from amqpstorm.tests.utility import FakeConnection
+# from amqpstorm import Channel
+# from pytest_rabbitmq import factories
+#
+# rabbitmq_my_proc = factories.rabbitmq_proc(port=5672, logsdir='/tmp')
+# rabbitmq_my = factories.rabbitmq('rabbitmq_my_proc')
 
 
 class MessagingTestCase(unittest.TestCase):
@@ -17,8 +19,8 @@ class MessagingTestCase(unittest.TestCase):
         pass
 
     @patch.object(Consumer, 'start')
-    def test_store_prov_called(self, mock):
-        """Test if store_provenance was called."""
-        CONSUMER = Consumer(broker['host'], broker['user'], broker['pass'], broker['queue'])
+    def test_start_consumer(self, mock):
+        """Test if start a consumer was called."""
+        CONSUMER = Consumer()
         CONSUMER.start()
         self.assertTrue(mock.called)
